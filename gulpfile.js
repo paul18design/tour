93403156638297e8c644cf48cbe971a277452dc3
paul18design <pablo_beltran15@outlook.com>
2019-04-23 12:29:56 -0500

    Add Home.html

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,25 @@ var gulp = require('gulp');
 var sass = require ('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
 gulp.task('sass', function(){
     return gulp.src('scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
 });
+
 
 gulp.task('watch', function(){
     gulp.watch('scss/**/*.scss', ['sass']);
 });
+
+
