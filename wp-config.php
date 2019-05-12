880fb9b59337a3fbd61dcbc9d577ffdb9e517c81
David Rodriguez <ceo@harcorp.com.co>
2019-05-11 22:16:59 -0500

    home final

--- wp-config.php
+++ wp-config.php
@@ -18,66 +18,53 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'tour');
+if ( file_exists( dirname(__FILE__) . '/wp-config-local.php' ) ) {
+    require_once ( dirname( __FILE__ ) . '/wp-config-local.php');
+} else {
+    // ** MySQL settings - You can get this info from your web host ** //
+    /** The name of the database for WordPress */
+    define('DB_NAME', 'database_name_here');
+    /** MySQL database username */
+    define('DB_USER', 'username_here');
+    /** MySQL database password */
+    define('DB_PASSWORD', 'password_here');
+    /** MySQL hostname */
+    define('DB_HOST', 'localhost');
+    /** Database Charset to use in creating database tables. */
+    define('DB_CHARSET', 'utf8');
+    /** The Database Collate type. Don't change this if in doubt. */
+    define('DB_COLLATE', '');
 
-/** MySQL database username */
-define('DB_USER', 'root');
+    /**#@+
+     * Authentication Unique Keys and Salts.
+     *
+     * Change these to different unique phrases!
+     * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+     * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+     *
+     * @since 2.6.0
+     */
+    define('AUTH_KEY',         'put your unique phrase here');
+    define('SECURE_AUTH_KEY',  'put your unique phrase here');
+    define('LOGGED_IN_KEY',    'put your unique phrase here');
+    define('NONCE_KEY',        'put your unique phrase here');
+    define('AUTH_SALT',        'put your unique phrase here');
+    define('SECURE_AUTH_SALT', 'put your unique phrase here');
+    define('LOGGED_IN_SALT',   'put your unique phrase here');
+    define('NONCE_SALT',       'put your unique phrase here');
 
-/** MySQL database password */
-define('DB_PASSWORD', '');
+    define('WP_DEBUG', true);
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+    /**#@-*/
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'dA,l@Oi741b9+<p-u(2fv>wSOIB@xK0*QKG+I(Z2= )qB3Yi=p951uJ|P ?$o[M&');
-define('SECURE_AUTH_KEY',  'Yc+j:fVT$S^:QA89B8tu &Qg&J?85q2-#?mo`]c7(3jInM [H>H*D{,%nA_,`Q>M');
-define('LOGGED_IN_KEY',    'ufQo$t9lVI3RL*IB^c7aCC.0mmLS_< 36tYj.Eh^%v >s- Wg|=Fs(mi{:R)`=@Q');
-define('NONCE_KEY',        '{ws+t/~FhYYr;Zw)Lx~aIU7?C_F&mu9Okza!$SfGu9??%go@i/,_tv}D8omj+,6>');
-define('AUTH_SALT',        'b2ZgJO+qAfQygEP>,J;e.l`zdc0yDbPC@W/o*p^ +DQ8sv-|p^r [miBm2$c6|*G');
-define('SECURE_AUTH_SALT', '{ICZBOunw9X,#~i6EN-(=WipLcM]fK,V2},K8R^8[YgQ)d5*P6uYAz !~`(u[>N*');
-define('LOGGED_IN_SALT',   '@%ms_/yVfGkYm]Fo|dF(#lQ6iL!tP3v!fQEh@%ZQ);Q}1]A_4kcxS)+AZD+,U?r9');
-define('NONCE_SALT',       '$}xb<3_5uyuqyZJa.]-Rxy%aS4kt/OyW&a*ePWsR|k*UV;Z*`j&^D?d P_O>s1^k');
-
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = 'wp9309_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
- */
-define('WP_DEBUG', false);
+    /**
+     * WordPress Database Table prefix.
+     *
+     * You can have multiple installations in one database if you give each
+     * a unique prefix. Only numbers, letters, and underscores please!
+     */
+    $table_prefix  = 'hc93_';
+}
 
 /* That's all, stop editing! Happy blogging. */
 
--- wp-content/themes/tour/home.php
+++ wp-content/themes/tour/home.php
@@ -37,51 +37,8 @@ if (!empty($sliders)): ?>
         </a>
     </div>
 <?php endif; ?>
-
-    <div class="description-bg">
-        <div class="container">
-            <div class="row">
-                <div class="text-center agane-bold gray-color fs_80 col-lg line-height_35 pt-60 pb-5">
-                    <em class="fs_26 agane-light">¿Qué nos hace</em> <br>
-                    diferentes?
-                </div>
-                <div class="text-center agane-light gray-color fs_20 pb-5">
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                    laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                    ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
-                    dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla
-                    facilisis at vero eros et accumsan et iusto odio dignissim qui blanditte feugait nulla facilisi.
-                </div>
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/4KnfmkIXmv8"
-                            allowfullscreen></iframe>
-                </div>
-            </div>
-        </div>
-        <div class="container pt-50 pb-50">
-            <div class="row pl-30">
-                <div class="col-sm position-relative">
-                    <div class="lateral-text position-absolute">TOUR DE FRANCIA</div>
-                    <div class="france-img"></div>
-                </div>
-                <div class="col-sm">
-                    <div class="white-color fs_60 agane-extra p-title line-height_50">NUESTROS DESTINOS</div>
-                    <hr class="line-red text-lef">
-                    <div class="white-color fs_18 agane-light pt-2">Lorem ipsum dolor sit amet, consectetuer adipiscing
-                        elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
-                    </div>
-                    <span>
-                        <a href="#" target="_blank"><svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                                    width="0.625in" height="0.139in">
-                           <path fill-rule="evenodd" fill="rgb(255, 255, 255)"
-                                 d="M41.114,-0.004 L40.331,0.634 L43.317,4.462 L0.003,4.462 L0.003,5.485 L43.317,5.485 L40.331,9.313 L41.114,9.950 L44.999,4.973 L41.114,-0.004 Z"/>
-                           </svg></a>
-                    </span>
-                </div>
-            </div>
-        </div>
-    </div>
+<?php
+ the_content();
+?>
 <?php
 get_footer();
